d2d1844f9d74ffb34cc49c292c6cab091d29fc82
Alejandro Solis <solis1492@gmail.com>
2014-08-26 09:07:42 -0600

    First commit

---
+++ src/HeapCollision/Main.java
@@ -0,0 +1,10 @@
+package HeapCollision;
+
+/**
+ * Created by Alejandro on 8/26/2014.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
