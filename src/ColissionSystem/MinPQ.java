b9d646b441ddb433fef7e8ac85414e58be287ae3
Alejandro Solis <solis1492@gmail.com>
2014-09-04 16:27:12 -0600

    Implemented Heap, CollisionSystem and Particle

---
+++ src/ColissionSystem/MinPQ.java
@@ -0,0 +1,125 @@
+package ColissionSystem;
+
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class MinPQ<Key> implements Iterable<Key> {
+    private Key[] pq;                    // store items at indices 1 to N
+    private int N;                       // number of items on priority queue
+    private Comparator<Key> comparator;  // optional comparator
+
+    public MinPQ(int initCapacity) {
+        pq = (Key[]) new Object[initCapacity + 1];
+        N = 0;
+    }
+
+    public MinPQ() {
+        this(1);
+    }
+
+    public MinPQ(int initCapacity, Comparator<Key> comparator) {
+        this.comparator = comparator;
+        pq = (Key[]) new Object[initCapacity + 1];
+        N = 0;
+    }
+
+    public boolean isEmpty() {
+        return N == 0;
+    }
+
+    public int size() {
+        return N;
+    }
+
+    private void resize(int capacity) {
+        assert capacity > N;
+        Key[] temp = (Key[]) new Object[capacity];
+        for (int i = 1; i <= N; i++) temp[i] = pq[i];
+        pq = temp;
+    }
+
+    public void insert(Key x) {
+        if (N == pq.length - 1) resize(2 * pq.length);
+
+        pq[++N] = x;
+        swim(N);
+        assert isMinHeap();
+    }
+
+    public Key delMin() {
+        if (isEmpty()) throw new NoSuchElementException("Priority queue underflow");
+        exch(1, N);
+        Key min = pq[N--];
+        sink(1);
+        pq[N+1] = null;         // avoid loitering and help with garbage collection
+        if ((N > 0) && (N == (pq.length - 1) / 4)) resize(pq.length  / 2);
+        assert isMinHeap();
+        return min;
+    }
+
+    private void swim(int k) {
+        while (k > 1 && greater(k/2, k)) {
+            exch(k, k/2);
+            k = k/2;
+        }
+    }
+
+    private void sink(int k) {
+        while (2*k <= N) {
+            int j = 2*k;
+            if (j < N && greater(j, j+1)) j++;
+            if (!greater(k, j)) break;
+            exch(k, j);
+            k = j;
+        }
+    }
+
+    private boolean greater(int i, int j) {
+        if (comparator == null) {
+            return ((Comparable<Key>) pq[i]).compareTo(pq[j]) > 0;
+        }
+        else {
+            return comparator.compare(pq[i], pq[j]) > 0;
+        }
+    }
+
+    private void exch(int i, int j) {
+        Key swap = pq[i];
+        pq[i] = pq[j];
+        pq[j] = swap;
+    }
+
+    private boolean isMinHeap() {
+        return isMinHeap(1);
+    }
+
+    private boolean isMinHeap(int k) {
+        if (k > N) return true;
+        int left = 2*k, right = 2*k + 1;
+        if (left  <= N && greater(k, left))  return false;
+        if (right <= N && greater(k, right)) return false;
+        return isMinHeap(left) && isMinHeap(right);
+    }
+
+    public Iterator<Key> iterator() { return new HeapIterator(); }
+
+    private class HeapIterator implements Iterator<Key> {
+        private MinPQ<Key> copy;
+
+        public HeapIterator() {
+            if (comparator == null) copy = new MinPQ<Key>(size());
+            else                    copy = new MinPQ<Key>(size(), comparator);
+            for (int i = 1; i <= N; i++)
+                copy.insert(pq[i]);
+        }
+
+        public boolean hasNext()  { return !copy.isEmpty();                     }
+        public void remove()      { throw new UnsupportedOperationException();  }
+
+        public Key next() {
+            if (!hasNext()) throw new NoSuchElementException();
+            return copy.delMin();
+        }
+    }
+}
---
+++ src/ColissionSystem/Particle.java
@@ -0,0 +1,127 @@
+package ColissionSystem;
+
+import java.awt.Color;
+
+public class Particle {
+    private static final double INFINITY = Double.POSITIVE_INFINITY;
+
+    private double rx, ry;    // position
+    private double vx, vy;    // velocity
+    private double radius;    // radius
+    private double mass;      // mass
+    private Color color;      // color
+    private int count;        // number of collisions
+
+
+    public Particle(double rx, double ry, double vx, double vy, double radius, double mass, Color color) {
+        this.vx = vx;
+        this.vy = vy;
+        this.rx = rx;
+        this.ry = ry;
+        this.radius = radius;
+        this.mass   = mass;
+        this.color  = color;
+    }
+
+    public Particle(boolean colorSelect) {
+        rx     = Math.random();
+        ry     = Math.random();
+        vx     = 0.01 * (Math.random() - 0.5);
+        vy     = 0.01 * (Math.random() - 0.5);
+        radius = 0.01;
+        mass   = 0.5;
+        if (colorSelect) {
+            color = Color.red;
+        }
+        else {
+            color = Color.blue;
+        }
+    }
+
+    public void move(double dt) {
+        rx += vx * dt;
+        ry += vy * dt;
+    }
+
+    public void draw() {
+        StdDraw.setPenColor(color);
+        StdDraw.filledCircle(rx, ry, radius);
+    }
+
+    public int count() { return count; }
+
+
+    public double timeToHit(Particle b) {
+        Particle a = this;
+        if (a == b) return INFINITY;
+        double dx  = b.rx - a.rx;
+        double dy  = b.ry - a.ry;
+        double dvx = b.vx - a.vx;
+        double dvy = b.vy - a.vy;
+        double dvdr = dx*dvx + dy*dvy;
+        if (dvdr > 0) return INFINITY;
+        double dvdv = dvx*dvx + dvy*dvy;
+        double drdr = dx*dx + dy*dy;
+        double sigma = a.radius + b.radius;
+        double d = (dvdr*dvdr) - dvdv * (drdr - sigma*sigma);
+        if (d < 0) return INFINITY;
+        return -(dvdr + Math.sqrt(d)) / dvdv;
+    }
+
+    public double timeToHitVerticalWall() {
+        if      (vx > 0) return (1.0 - rx - radius) / vx;
+        else if (vx < 0) return (radius - rx) / vx;
+        else             return INFINITY;
+    }
+
+    public double timeToHitHorizontalWall() {
+        if      (vy > 0) return (1.0 - ry - radius) / vy;
+        else if (vy < 0) return (radius - ry) / vy;
+        else             return INFINITY;
+    }
+
+    public void bounceOff(Particle that) {
+        double dx  = that.rx - this.rx;
+        double dy  = that.ry - this.ry;
+        double dvx = that.vx - this.vx;
+        double dvy = that.vy - this.vy;
+        double dvdr = dx*dvx + dy*dvy;             // dv dot dr
+        double dist = this.radius + that.radius;   // distance between particle centers at collison
+
+        double F = 2 * this.mass * that.mass * dvdr / ((this.mass + that.mass) * dist);
+        double fx = F * dx / dist;
+        double fy = F * dy / dist;
+
+        this.vx += fx / this.mass;
+        this.vy += fy / this.mass;
+        that.vx -= fx / that.mass;
+        that.vy -= fy / that.mass;
+
+        this.count++;
+        that.count++;
+
+        if(this.color == that.color){
+            this.radius = this.radius <= 0.3 ? this.radius * 1.1 : 0.3;
+            this.mass *= 1.1;
+            that.radius = that.radius <= 0.3 ? that.radius * 1.1 : 0.3;
+            that.mass *= 1.1;
+        } else{
+            if(this.radius > that.radius)
+                that.color = this.color;
+            else if (this.radius < that.radius)
+                this.color = that.color;
+        }
+    }
+
+    public void bounceOffVerticalWall() {
+        vx = -vx;
+        count++;
+    }
+
+    public void bounceOffHorizontalWall() {
+        vy = -vy;
+        count++;
+    }
+
+    public double kineticEnergy() { return 0.5 * mass * (vx*vx + vy*vy); }
+}
